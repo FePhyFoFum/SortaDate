b4123eb055f23808759569fd4bd01a5874e52ea9
Stephen Smith <blackrim@gmail.com>
2017-01-20 17:02:29 -0500

    first update

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # SortaDate
+
+## Introduction
+This repository contains scripts that you can use at different stages to attempt to find more clock-like genes. Generally, you would use these for dating analyses with another package. 
+
+## Requirements
+You will minimally need `python` to run the scripts. Other than `python`, depending on how much you want to run within _SortaData_. 
+
+The expectation of input are a series of genes that have been aligned and for which there are gene trees. Generally, we would expect these to be rooted as well. 
+
+## Usage
+With the input of a directory of gene alignments and corresponding gene trees, the steps of these analyses include 
+1. Get the tip-to-root variation with the `get_var_length.py`
+
+### Rooting
+If you need trees to be rooted, you can perform this in many different ways. One way that you can root these trees is with `phyx` (found [https://github.com/FePhyFoFum/phyx][1]). 
+
+### Conducting clock analyses
+If you would like to conduct likelihood ratio tests for the clock for each of the gene trees, you may do this with `paup` and you can find that here [http://people.sc.fsu.edu/\~dswofford/paup\_test/][2]. There are scripts included in `SortaDate` to conduct these analyses that will add the necessary paup block to the file.
+
+## Citations
+The scripts and procedures discussed here are presented in 
+
+[1]:	https://github.com/FePhyFoFum/phyx
+[2]:	http://people.sc.fsu.edu/~dswofford/paup_test/
---
+++ src/get_var_length.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import subprocess
+import argparse as ap
+
+flend = ".tre"
+outg = ""
+cmd1 = "pxrmt -t TREE -n "+outg+"| pxlstr"
+cmd2 = "pxlstr -t TREE"
+writetofile = False
+
+if __name__ == "__main__":
+    par = ap.ArgumentParser(description="SortaDate: get_var_length | This will calculate the variation in the tip to root length for each gene tree")
+    par.add_argument('di', type=str, nargs=1,metavar='DIR',help="directory where the gene trees are")
+    par.add_argument('--flend', dest='fileending',help="the file endings for the gene trees")
+    par.add_argument('--outg', dest='outgroups',default="",help="outgroups separated by commas")
+    par.add_argument('--loc', dest='phyx_location',default="",help="where are pxlstr (and pxrmt if you have outgroups)")
+    par.add_argument('--outf',dest='outf',help="the outfile")
+
+    #parse the arguments
+    args = par.parse_args()
+    if args.fileending != None:
+        flend = args.fileending
+    di = args.di[0]
+    print "directory:",di
+    print "file ending for trees:",flend
+    print "outgroups:",args.outgroups
+    if args.outgroups != "":
+        outg = args.outgroups
+        cmd = cmd1
+    else:
+        cmd = cmd2
+    print "phyx location:",args.phyx_location
+    if args.phyx_location != "":
+        cmd = args.phyx_location+"/"+cmd
+    if args.outf != None:
+        print "outfile:",args.outf
+        writetofile = True
+        outf = open(args.outf,'w')
+
+    #run the script
+    for i in os.listdir(di):
+        if i[-len(flend):] == flend:
+            fd = di+"/"+i
+            p = subprocess.Popen(cmd.replace("TREE",fd),shell=True,stdout=subprocess.PIPE)
+            x = p.communicate()[0].split("\n")
+            var = ""
+            tl = ""
+            for j in x:
+                if len(j) < 3:
+                    continue
+                spls = j.split()
+                if spls[0] == "treelength:":
+                    tl = spls[1]
+                elif spls[0] == "rttipvar:":
+                    var = spls[1]
+            if writetofile == False:
+                print i+"\t"+var+"\t"+tl
+            else:
+                outf.write(i+"\t"+var+"\t"+tl+"\n")
+    if writetofile == True:
+        outf.close()
